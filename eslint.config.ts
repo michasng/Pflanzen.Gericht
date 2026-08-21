e2c34aea47bd4e54e16037ed5c4acd4c001f8177
Micha Sengotta <micha.sengotta@tngtech.com>
2026-08-21 16:15:37 +0200

    chore: Exclude supabase/.temp files from eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -11,7 +11,7 @@ export default defineConfigWithVueTs(
     files: ['**/*.{vue,ts,mts,tsx}'],
   },
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/supabase/.temp/**']),
 
   ...pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
